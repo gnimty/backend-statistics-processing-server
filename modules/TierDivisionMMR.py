08e01afcc2fd5b8eb6daaf0e7b2d071a19da2f68
so1omon <solmin3665@gmail.com>
2023-08-16 15:11:32 +0900

    feat: 리그엔트리 컬렉션 deprecated, summoner 컬렉션에서 랭킹조회를 위해 mmr 필드 추가 및 ENUM class 생성

---
+++ modules/TierDivisionMMR.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class MMR(Enum):
+    challenger = 2800
+    grandmaster = 2800
+    master = 2800
+    diamond = 2400
+    emerald = 2000
+    platinum = 1600
+    gold = 1200
+    silver = 800
+    bronze = 400
+    iron = 0
