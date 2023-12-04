feb4ae94f2bcd318ed7d3fe7f1062ef83653d488
so1omon <solmin3665@gmail.com>
2023-12-04 19:01:21 +0900

    test path add#7

--- modules/league_entries.py
+++ modules/league_entries.py
@@ -76,10 +76,11 @@ def collect_all_summoners():
   
   entries = []
 
+  for queue in queues:
   # TODO 이후 master 하위 리그까지 전부 업데이트해야 함
-  entries.extend(league_exp_v4.get_top_league("challengerleagues",queue = queue) )
-  entries.extend(league_exp_v4.get_top_league("grandmasterleagues", queue = queue))
-  entries.extend(league_exp_v4.get_top_league("masterleagues", queue = queue))
+    entries.extend(league_exp_v4.get_top_league("challengerleagues",queue = queue) )
+    entries.extend(league_exp_v4.get_top_league("grandmasterleagues", queue = queue))
+    entries.extend(league_exp_v4.get_top_league("masterleagues", queue = queue))
   
   for entry in entries:
     summoner.update_by_summoner_brief(entry)
